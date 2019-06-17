56745e467052c798310c49b3cc28ac0c1e2a7c80
Alexander Barth <barth.alexander@gmail.com>
2019-06-17 15:09:59 +0200

    udpate doc

--- src/CFTime.jl
+++ src/CFTime.jl
@@ -615,6 +615,26 @@ not possible for the other calendars.
 | `all_leap`, `366_day`   | `DateTimeAllLeap`             | `DateTimeAllLeap`              |
 | `360_day`             | `DateTime360Day`              | `DateTime360Day`               |
 
+## Example:
+
+```julia
+using CFTime, Dates
+# standard calendar
+dt = CFTime.timedecode([0,1,2,3],"days since 2000-01-01 00:00:00")
+# 4-element Array{Dates.DateTime,1}:
+#  2000-01-01T00:00:00
+#  2000-01-02T00:00:00
+#  2000-01-03T00:00:00
+#  2000-01-04T00:00:00
+
+dt = CFTime.timedecode([0,1,2,3],"days since 2000-01-01 00:00:00","360_day")
+# 4-element Array{DateTime360Day,1}:
+#  DateTime360Day(2000-01-01T00:00:00)
+#  DateTime360Day(2000-01-02T00:00:00)
+#  DateTime360Day(2000-01-03T00:00:00)
+#  DateTime360Day(2000-01-04T00:00:00)
+```
+
 """
 function timedecode(data,units,calendar = "standard"; prefer_datetime = true)
     DT = timetype(calendar)
