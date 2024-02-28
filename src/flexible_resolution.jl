3ff58b35efce965cd5259e9778e432dcfc8dc1c0
Alexander Barth <barth.alexander@gmail.com>
2024-02-28 14:28:32 +0100

    doc string

--- src/flexible_resolution.jl
+++ src/flexible_resolution.jl
@@ -205,6 +205,18 @@ for (CFDateTime,calendar) in [(:DateTimeStandard,"standard"),
             return DT{T,Torigintuple}(Δ)
         end
 
+        """
+    $($CFDateTime)([Ti::DataType], y, [m, d, h, mi, s, ms]) -> $($CFDateTime)
+
+Construct a `$($CFDateTime)` type by year (`y`), month (`m`, default 1),
+day (`d`, default 1), hour (`h`, default 0), minute (`mi`, default 0),
+second (`s`, default 0), millisecond (`ms`, default 0).
+All arguments must be convertible to `Int64`.
+`$($CFDateTime)` is a subtype of `AbstractCFDateTime`.
+
+The netCDF CF calendars are defined in [the CF Standard](http://cfconventions.org/cf-conventions/cf-conventions.html#calendar).
+This type implements the calendar defined as "$($calendar)".
+        """
         function $CFDateTime(Ti::DataType,
                              args...;
 #                             origin = (1858,11,17),
