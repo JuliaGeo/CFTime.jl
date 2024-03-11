d0a51746a5e9187cc1279f28c15c93867a201d48
Alexander Barth <barth.alexander@gmail.com>
2024-03-11 23:06:48 +0100

    better accuracy if dt.instant is a Float

--- src/rounding.jl
+++ src/rounding.jl
@@ -2,33 +2,48 @@
 
 
 function round(::Type{DateTime}, dt::AbstractCFDateTime,r = RoundNearestTiesUp)
+    function round_ms(t)
+        t_ms =
+            if 3+_exponent(t) > 0
+                t.duration * _factor(t) * 10^(3+_exponent(t))
+            else
+                t.duration * _factor(t) / 10^(-3-_exponent(t))
+            end
+
+        t_ms_rounded =
+            if t_ms isa Integer
+                Int64(t_ms)
+            else
+                round(Int64,t_ms,r)
+            end
+        return Dates.Millisecond(t_ms_rounded)
+    end
+
+    origintuple = _origintuple(dt)
     origin = _origin_period(dt)
-    t = dt.instant + (origin + DATETIME_OFFSET)
 
-    t_ms =
-        if 3+_exponent(t) > 0
-            t.duration * _factor(t) * 10^(3+_exponent(t))
-        else
-            t.duration * _factor(t) / 10^(-3-_exponent(t))
-        end
+    if length(origintuple) >= 7
+        # origin needs to be taken into account when rounding
+        duration = round_ms(dt.instant + origin) + DATETIME_OFFSET
+    else
+        # better accuracy if dt.instant is a Float
+        duration = round_ms(dt.instant) + origin + DATETIME_OFFSET
+    end
 
-    t_ms_rounded = round(Int64,t_ms,r)
-    return DateTime(UTInstant{Millisecond}(Dates.Millisecond(t_ms_rounded)))
+    return DateTime(UTInstant{Millisecond}(Dates.Millisecond(duration)))
+
 end
 
 
+# TODO: make generic
 function Base.floor(dt::DateTimeStandard,p::Period)
     origintuple = _origintuple(dt)
     origin = _origin_period(dt)
     t = dt.instant + origin
-
-    tt = t - mod(t,p)
-
-    ttt = tt - origin
-
-    return DateTimeStandard{typeof(ttt), Val(origintuple)}(ttt)
+    t_mod = (t - mod(t,p)) - origin
+    return DateTimeStandard{typeof(t_mod), Val(origintuple)}(t_mod)
 end
 
-function Base.floor(dt::DateTimeStandard,p::Dates.TimePeriod)
+function Base.floor(dt::AbstractCFDateTime,p::Dates.TimePeriod)
     floor(dt,convert(Period,p))
 end
--- test/test_rounding.jl
+++ test/test_rounding.jl
@@ -8,8 +8,17 @@ using CFTime: DateTimeStandard
 dt = DateTimeStandard(24*60*60*1000*1000 + 123,"microsecond since 2000-01-01")
 @test round(DateTime,dt) == DateTime(2000,1,2)
 
-dt = DateTimeStandard(24*60*60,"second since 2000-01-01")
+dt = DateTimeStandard(1,"days since 2000-01-01")
+@test round(DateTime,dt) == DateTime(2000,1,2)
 
+# rounding with low-precision float
+dt = DateTimeStandard(1.f0,"days since 2000-01-01")
+@test round(DateTime,dt) == DateTime(2000,1,2)
+
+dt = DateTimeStandard(24*60*60,"second since 2000-01-01")
 @test floor(dt+Second(9),Second(10)) == dt
 @test round(dt+Second(9),Second(10)) == dt + Second(10)
 
+
+dt = DateTimeStandard(24*60*60,"second since 2000-01-01 00:00:00.111222")
+@test round(DateTime,dt) == DateTime(2000,1,2,0,0,0,111)
