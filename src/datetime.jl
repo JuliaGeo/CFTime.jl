e83f4a1be1b6975d516096d55420c3aba772b86b
Alexander Barth <barth.alexander@gmail.com>
2024-06-21 13:25:57 +0200

    revert specialization

--- src/datetime.jl
+++ src/datetime.jl
@@ -191,9 +191,9 @@ end
      (_origin_period(dt1) - _origin_period(dt2)) + (dt1.instant - dt2.instant)
 end
 
-@inline function -(dt1::AbstractCFDateTime{T,Torigintuple},dt2::AbstractCFDateTime{T,Torigintuple}) where {T,Torigintuple}
-     dt1.instant - dt2.instant
-end
+#@inline function -(dt1::AbstractCFDateTime{T,Torigintuple},dt2::AbstractCFDateTime{T,Torigintuple}) where {T,Torigintuple}
+#     dt1.instant - dt2.instant
+#end
 
 
 function -(dt1::AbstractCFDateTime,dt2::DateTime)
