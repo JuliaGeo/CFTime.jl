baaabe351d732c70b25f7a04dba253b72a8794b1
Alexander Barth <barth.alexander@gmail.com>
2026-03-11 16:53:23 +0100

    commit simplify Dates.len

--- src/ranges.jl
+++ src/ranges.jl
@@ -1,9 +1,5 @@
 function Dates.len(first::T, last::T, step::DT) where {T <: AbstractCFDateTime} where
-    {DT <: Union{Dates.Day, Dates.Hour, Dates.Minute, Dates.Second, Dates.Millisecond}}
-    return Dates.value(last - first) ÷ Dates.value(Dates.Millisecond(step))
-end
-
-function Dates.len(first::T, last::T, step::Period) where {T <: AbstractCFDateTime}
+    {DT <: Union{Period, Dates.Day, Dates.Hour, Dates.Minute, Dates.Second, Dates.Millisecond}}
     if Dates.value(step) == 0
         throw(ArgumentError("step cannot be zero"))
     end
